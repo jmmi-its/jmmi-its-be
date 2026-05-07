398037200ac452f8346021a06ee75b399fad1fcc
nawa316 <muhammadadedzakwan@gmail.com>
2026-05-07 10:07:00 +0700

    feat: add table migration

---
+++ prisma/migrations/20260507030642_create_short_links_table/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `click_count` on the `links` table. All the data in the column will be lost.
+  - You are about to drop the column `short_code` on the `links` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "links_category_id_idx";
+
+-- DropIndex
+DROP INDEX "links_short_code_key";
+
+-- AlterTable
+ALTER TABLE "links" DROP COLUMN "click_count",
+DROP COLUMN "short_code";
+
+-- CreateTable
+CREATE TABLE "short_links" (
+    "id" TEXT NOT NULL,
+    "short_code" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "click_count" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "short_links_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "short_links_short_code_key" ON "short_links"("short_code");
